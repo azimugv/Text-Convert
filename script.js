143d8de2065636acdd875b6927766894b3e8e3a1
Md. Abdullah Al Azim <67109691+azimugv@users.noreply.github.com>
2022-08-28 23:59:16 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const textarea = document.querySelector('textarea'),
+fileNamwInput = document.querySelector(".file-name input"),
+selectMenu = document.querySelector('.save-as select'),
+saveBtn  = document.querySelector(".save-btn");
+
+
+selectMenu.addEventListener("change", () => {
+    let selectedOption = selectMenu.options[selectMenu.selectedIndex].text;
+    saveBtn.innerText = `Save as ${selectedOption.split(" ")[0]} File`;
+});
+
+
+saveBtn.addEventListener('click', () => {
+    const blob = new Blob([textarea.value], {type: selectMenu.value});
+    const fileUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = fileNamwInput.value;
+    link.href = fileUrl;
+    link.click();
+} );
